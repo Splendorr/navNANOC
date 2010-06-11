d9101586787ae2303a17a43bfb28a20d828b315a
Eos <eos@eos.local>
2010-06-10 22:43:45 -0400

    Update Contact.html

--- output/j/jq_cycle.js
+++ output/j/jq_cycle.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $('#quote_cycle').cycle({
-		fx: 'fade' // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+		fx: 'fade',
+		timeout: 7000,
+		speed: 1500
 	});
 });
