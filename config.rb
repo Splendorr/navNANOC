ae5ec89866621acf4057de4f62446ad7cf6125d1
N. O. Splendorr <simolinic@gmail.com>
2010-06-14 04:17:56 -0400

    Everything is in place to use Compass/Susy; now just need to get the hang of Susy.

---
+++ config.rb
@@ -0,0 +1,15 @@
+require 'susy'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+
+project_path    = File.dirname(__FILE__)
+http_path       = "/"
+##output_style    = :compressed
+sass_dir        = "content/s"
+css_dir         = "output/s"
+images_dir      = "i"
+javascripts_dir = "j"
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
---
+++ output/j/jq_cycle.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#quote_cycle').cycle({
+		fx: 'fade',
+		timeout: 7000,
+		speed: 1500
+	});
+});
