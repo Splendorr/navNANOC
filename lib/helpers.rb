bc82a56e72d017f6620cbacf31b3e1e09cca8cdb
N. O. Splendorr <simolinic@gmail.com>
2010-06-15 00:29:20 -0400

    Removed old files prepping to change Susified to master.

--- lib/helpers.rb
+++
@@ -1,3 +0,0 @@
-include Nanoc3::Helpers::Rendering
-include Nanoc3::Helpers::Filtering
-include Nanoc3::Helpers::LinkTo
--- output/j/jq_cycle.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('#quote_cycle').cycle({
-		fx: 'fade',
-		timeout: 7000,
-		speed: 1500
-	});
-});
